a332e224638512f5100e38b83bcb91c7db06ac7f
Takuma Hanatani <mail@potato4d.me>
2017-03-23 01:02:34 +0900

    メニューを実装

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import "./Navigation.css";
+import {Icon} from 'react-fa'
 
 class Navigation extends Component {
   render() {
     return (
-      <header className="Navigation">
-        <nav>
-
-        </nav>
-      </header>
+      <nav className="Navigation">
+        <ul>
+          <li><Icon name='file-text-o' /></li>
+          <li><Icon name='folder-open-o' /></li>
+          <li><Icon name='code' /></li>
+          <li><Icon name='share-alt' /></li>
+          <li><Icon name='gear' /></li>
+        </ul>
+      </nav>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+require('rodal/lib/rodal.css');
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
